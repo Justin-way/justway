f4ca6fde068b7175e8ae2c2bcf07e84c3036e203
Wayne Yeh <thebest77771023@yahoo.com.tw>
2022-12-24 14:08:37 +0800

    finish all

---
+++ blog/exampleSite/static/js/utterances.js
@@ -0,0 +1,11 @@
+(function() {
+  var script = document.createElement('script');
+  script.src = 'https://utteranc.es/client.js';
+  script.setAttribute('repo', 'Justin-way/Justin-way.github.io');
+  script.setAttribute('issue-term', 'pathname');
+  script.setAttribute('label', 'blog-comment');
+  script.setAttribute('theme', 'github-light');
+  script.setAttribute('crossorigin', 'anonymous');
+  script.setAttribute('async', 'true');
+  document.getElementsByTagName('body')[0].appendChild(script);
+})();
