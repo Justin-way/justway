aa1d7caf1ee4d0a9d3036940e408dab31a3694a8
Wayne Yeh <thebest77771023@yahoo.com.tw>
2023-01-02 23:03:11 +0800

    add

--- blog/exampleSite/static/js/utterances.js
+++ blog/exampleSite/static/js/utterances.js
@@ -1,7 +1,7 @@
 (function() {
   var script = document.createElement('script');
   script.src = 'https://utteranc.es/client.js';
-  script.setAttribute('repo', 'Justin-way/Justin-way.github.io');
+  script.setAttribute('repo', 'Justin-way/justway');
   script.setAttribute('issue-term', 'pathname');
   script.setAttribute('label', 'blog-comment');
   script.setAttribute('theme', 'github-light');
